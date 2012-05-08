5557fe3a4078a52cacb504639ef4a08487d6e2e2
Tatsh <ddrtist@gmail.com>
2012-05-08 10:30:15 -0500

    Add files

---
+++ reset-password-final-form.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (c) 2011 Poluza.
+ * @author Andrew Udvare [au] <andrew@poluza.com>
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
+ * @package SutraTemplate
+ * @link http://www.example.com/
+ *
+ * @version 1.0
+ *
+ * $csrf
+ * $time_left_text
+ * $user - User object
+ */
+?>
+<header class="general"><h2>Change Password</h2></header>
+<form id="reset-password-final-form" method="post" action="/reset-password/<?php print $user->encodeUserId(); ?>/post">
+  <?php /*<p class="message"><?php print $time_left_text; ?></p>*/ ?>
+
+  <div class="form-textfield-container">
+    <label for="edit-password"><?php print __('Password:'); ?> <span class="form-required-marker">*</span></label>
+    <input id="edit-password" class="form-password form-textfield" type="password" name="user_password" required pattern="^.{8,}$" autocomplete="off">
+  </div>
+
+  <div class="form-textfield-container">
+    <label for="edit-confirm-password"><?php print __('Confirm Password:'); ?> <span class="form-required-marker">*</span></label>
+    <input id="edit-confirm-password" class="form-password form-textfield" type="password" name="user_password2" required pattern="^.{8,}$" autocomplete="off">
+  </div>
+
+  <input type="hidden" value="<?php print $csrf; ?>" name="csrf">
+
+  <div class="form-ops-container">
+    <input type="submit" class="form-submit" value="<?php print __('Change Password'); ?>" name="op">
+    <input type="submit" class="form-submit" value="<?php print __('Cancel'); ?>" name="op">
+  </div>
+</form>
